5036d8ef2e73dc32f83fa4b6d411a9f8731014fc
croketillo <jose_carrizosa@hotmail.com>
2025-02-17 07:56:07 +0100

    Last commit

--- setup.py
+++ setup.py
@@ -14,9 +14,15 @@ setup(
     url="https://github.com/croketillo/tinyprogress",  # Cambia por la URL de tu repositorio
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',  
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules', 
+        'Topic :: Utilities'
     ],
     python_requires='>=3.6',
 )
